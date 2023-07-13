f06862f30c88876a0668c401b35188855e6db618
YiFangLu <technology99x@gmail.com>
2023-07-13 12:56:38 +0000

    Init repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Grid2 container>
+        <Grid2 xs={12}></Grid2>
+       </Grid2>
+    </div>
+  );
+}
+
+export default App;
