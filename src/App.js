b081ef498b81c66a543175a7c85b2ca9a2c3eea2
YiFangLu <technology99x@gmail.com>
2023-07-28 09:18:08 +0000

    Add new game button Add flag Fix new game Add trigger Add auto trigger Add flag

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import "./App.css";
 import _ from "lodash";
 import { useEffect, useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
 
 const cellSize = 30;
 
 function App() {
   const [boardInfo, setBoardInfo] = useState({
-    width: 26,
-    height: 19,
-    mine: 81,
+    width: 10,
+    height: 9,
+    mine: 9,
+    size: "large",
   });
   const [realBoard, setRealBoard] = useState([]);
   const [displayBoard, setDisplayBoard] = useState([]);
+  const [title, setTitle] = useState("");
 
   var leftClick = false;
   var rightClick = false;
@@ -23,26 +25,38 @@ function App() {
   };
 
   const getDims = (index) => {
-    const y = index / boardInfo.width;
+    const y = Math.floor(index / boardInfo.width);
     return { x: index % boardInfo.width, y: y };
   };
   const newGame = () => {
     const boardSize = boardInfo.width * boardInfo.height;
-    const mineList = _.sampleSize(_.range(boardSize, boardInfo.mine));
+    const mineList = _.sampleSize(_.range(boardSize), boardInfo.mine);
+
+    setTitle("new game");
 
     let newRealBoard = [];
 
-    _.each(_.range(boardInfo.width), (x) => {
-      _.each(_.range(boardInfo.height), (y) => {
+    _.each(_.range(boardInfo.height), (y) => {
+      _.each(_.range(boardInfo.width), (x) => {
         const index = getIndex(x, y);
         if (_.includes(mineList, index)) {
           newRealBoard.push("x");
           return;
         }
 
+        let minX = x - 1;
+        if (minX < 0) minX = 0;
+        let maxX = x + 2;
+        if (maxX > boardInfo.width) maxX = boardInfo.width;
+
+        let minY = y - 1;
+        if (minY < 0) minY = 0;
+        let maxY = y + 2;
+        if (maxY > boardInfo.height) maxY = boardInfo.height;
+
         let surroundArea = [];
-        _.each(_.range(x - 1, x + 2), (_x) => {
-          _.each(_.range(y - 1, y + 2), (_y) => {
+        _.each(_.range(minY, maxY), (_y) => {
+          _.each(_.range(minX, maxX), (_x) => {
             if (_x === x && _y === y) return;
 
             const _index = getIndex(_x, _y);
@@ -50,7 +64,7 @@ function App() {
           });
         });
 
-        const mineCount = _.intersection(mineList, surroundArea);
+        const mineCount = _.intersection(mineList, surroundArea).length;
 
         newRealBoard.push(mineCount);
       });
@@ -60,35 +74,143 @@ function App() {
     setDisplayBoard(Array(boardSize).fill("untrigger"));
   };
 
-  const trigger = () => {
-    console.log("left");
+  const trigger = (index, newDisplayBoard) => {
+    if (!newDisplayBoard) {
+      newDisplayBoard = [...displayBoard];
+    }
+
+    const currentValue = realBoard[index];
+    if (currentValue === "x") {
+      gameOver();
+      return;
+    }
+
+    if (currentValue !== 0) {
+      newDisplayBoard[index] = "number";
+      setDisplayBoard(newDisplayBoard);
+      return;
+    }
+
+    const { x, y } = getDims(index);
+    spread(x, y, newDisplayBoard);
+    setDisplayBoard(newDisplayBoard);
+    return newDisplayBoard;
   };
 
-  const triggerFlag = () => {
-    console.log("right");
+  const triggerFlag = (index) => {
+    if (displayBoard[index] === "number") return;
+
+    const newDisplayBoard = [...displayBoard];
+
+    let classValue = "flag";
+    if (newDisplayBoard[index] === "flag") classValue = "untrigger";
+    newDisplayBoard[index] = classValue;
+    setDisplayBoard(newDisplayBoard);
   };
 
-  const autoSpread = () => {
-    console.log("both");
+  const spread = (x, y, newDisplayBoard) => {
+    let spreadQueue = [];
+
+    const nextGrid = (x, y) => {
+      if (x < 0) return;
+      if (x >= boardInfo.width) return;
+
+      if (y < 0) return;
+      if (y >= boardInfo.height) return;
+
+      const index = getIndex(x, y);
+
+      if (_.includes(spreadQueue, index)) return;
+      spreadQueue.push(index);
+
+      if (realBoard[index] !== 0 && displayBoard[index] !== "flag") {
+        newDisplayBoard[index] = "number";
+        return;
+      }
+
+      if (displayBoard[index] !== "flag") {
+        newDisplayBoard[index] = "safe";
+      }
+
+      nextGrid(x - 1, y - 1);
+      nextGrid(x, y - 1);
+      nextGrid(x + 1, y - 1);
+      nextGrid(x - 1, y);
+      nextGrid(x + 1, y);
+      nextGrid(x - 1, y + 1);
+      nextGrid(x, y + 1);
+      nextGrid(x + 1, y + 1);
+    };
+
+    nextGrid(x, y);
   };
 
-  const gameOver = () => {};
+  const autoSpread = (index) => {
+    const { x, y } = getDims(index);
 
-  const handleTrigger = _.debounce(() => {
+    let minX = x - 1;
+    if (minX < 0) minX = 0;
+    let maxX = x + 2;
+    if (maxX > boardInfo.width) maxX = boardInfo.width;
+
+    let minY = y - 1;
+    if (minY < 0) minY = 0;
+    let maxY = y + 2;
+    if (maxY > boardInfo.height) maxY = boardInfo.height;
+
+    let flagCount = 0;
+    let mineCount = 0;
+    let surroundArea = [];
+    _.each(_.range(minY, maxY), (_y) => {
+      _.each(_.range(minX, maxX), (_x) => {
+        if (_x === x && _y === y) return;
+
+        const _index = getIndex(_x, _y);
+        if (displayBoard[_index] === "flag") flagCount++;
+        if (realBoard[_index] === "x") mineCount++;
+
+        surroundArea.push(_index);
+      });
+    });
+
+    if (flagCount !== mineCount) {
+      return;
+    }
+
+    let newDisplayBoard = [...displayBoard];
+
+    _.each(surroundArea, (_index) => {
+      if (displayBoard[_index] !== "untrigger") return;
+      trigger(_index, newDisplayBoard);
+    });
+  };
+
+  const gameOver = () => {
+    setTitle("game over");
+    setDisplayBoard(
+      _.map(_.range(boardInfo.height * boardInfo.width), (index) => {
+        if (realBoard[index] === "x") return "mine";
+        if (realBoard[index] === 0) return "safe";
+        return "number";
+      }),
+    );
+  };
+
+  const handleTrigger = _.debounce((index) => {
     if (leftClick && rightClick) {
-      autoSpread();
+      autoSpread(index);
       leftClick = false;
       rightClick = false;
       return;
     }
 
     if (leftClick) {
-      trigger();
+      trigger(index);
       leftClick = false;
       return;
     }
 
-    triggerFlag();
+    triggerFlag(index);
     rightClick = false;
   }, 100);
 
@@ -98,7 +220,8 @@ function App() {
     } else if (e.button === 2) {
       rightClick = true;
     }
-    handleTrigger();
+
+    handleTrigger(e.target.getAttribute("index"));
   };
 
   useEffect(() => {
@@ -117,8 +240,14 @@ function App() {
   return (
     <div className="App">
       <main className="full-size">
-        <Grid2 container className="full-size">
-          <Grid2 xs={12}>Some Titles</Grid2>
+        <Grid2
+          container
+          className="full-size flex-vertical-center"
+          rowSpacing={2}
+        >
+          <Grid2 xs={12} className="board-title">
+            {title}
+          </Grid2>
           <Grid2 xs={12} container>
             <Box className="full-size flex-center">
               <Grid2
@@ -141,14 +270,25 @@ function App() {
                       }}
                       xs={1}
                     >
-                      <div className={value} onMouseDown={handleClick}>
-                        {index}
+                      <div
+                        className={value}
+                        onMouseDown={handleClick}
+                        index={index}
+                      >
+                        {value === "flag" ? "" : realBoard[index]}
                       </div>
                     </Grid2>
                   );
                 })}
               </Grid2>
             </Box>
+          </Grid2>
+          <Grid2 xs={12}>
+            <Container maxWidth="sm" className="flex-end">
+              <Button variant="contained" onClick={() => newGame()}>
+                new game
+              </Button>
+            </Container>
           </Grid2>
         </Grid2>
       </main>
